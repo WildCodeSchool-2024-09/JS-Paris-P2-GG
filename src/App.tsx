a7d7f26dd3141983e4fb7a64515d56da79a34bc0
sidimohamed-tech <sidimed.sidimed.147@gmail.com>
2024-11-13 14:28:42 +0100

    Update App.tsx
    clean app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,6 @@ function App() {
 	return (
 		<section>
 			<NavBar />
-			<div className="cards">
-				<img src="src/assets/Lassana-removebg-final.png" alt="Lassana" />
-				<h2>Lassana</h2>
-				<h3>GG Ca !</h3>
-				<p>
-					<b>Catégorie :</b> Maison
-				</p>
-				<p>
-					<b>Prix:</b> 1 000 000 €
-				</p>
-			</div>
 			<Footer />
 		</section>
 	);
