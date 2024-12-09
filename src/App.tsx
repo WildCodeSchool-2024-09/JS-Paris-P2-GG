ca0e15e1b4cd26c345b19d2b1e69e13a9431dfa2
Claire-Cellier <c.cellier@gmail.com>
2024-12-09 18:09:29 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { AnswersProvider } from "./context/AnswersContext";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/footer";
+import ResponsiveAlert from './components/ResponsiveAlert';
 
 function App() {
 	return (
+		<ResponsiveAlert />
 		<AnswersProvider>
 			<NavBar />
 			<Outlet />
